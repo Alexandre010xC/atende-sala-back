8c2aa6cacf988dd9ada6399b4b87ec2dd276c7a4
Alexandre Coelho <alexandre.010c@gmail.com>
2023-11-28 15:17:26 -0300

    ask/cancel assistance and update status on queue

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -50,13 +50,13 @@ class ClassroomController extends Controller
         if(!Auth::attempt(['token_id' => $token_id, 'password' => $token_id])){
             return response()->json(['message' => 'user not found'], 401);
         }
-        $user = Auth::user()->id;
+        $user = Auth::user();
 
         $classroom = Classroom::where('id','=',$id)->first();
 
         $studentPresence = new StudentsClassroom();
         $studentPresence->classroom_id = $classroom->id;
-        $studentPresence->student_id = $user;
+        $studentPresence->student_id = $user->id;
         $studentPresence->seat = $request->seat;
 
         $studentPresence->save();
@@ -64,4 +64,59 @@ class ClassroomController extends Controller
 
         return response()->json($studentPresence, 200);
     }
+
+    public function askAssistance(Request $request)
+    {
+        $token_id = $request->token_id;
+        if(!Auth::attempt(['token_id' => $token_id, 'password' => $token_id])){
+            return response()->json(['message' => 'user not found'], 401);
+        }
+        $user = Auth::user();
+
+
+        $asked = QueueRequest::where('student_id','=',$user->id)->first();
+        if($asked){
+            $asked->destroy();
+            return response()->join(['position' => 'null'], 200);
+        }
+
+        $presence = StudentsClassroom::where('student_id','=',$user->id)->first();
+
+        $assistance = new QueueRequest();
+        $assistance->classroom_id = $presence->classroom_id;
+        $assistance->student_id = $presence->student_id;
+        $assistance->requested_at = Carbon::now();
+
+        $assistance->save();
+        $assistance->refresh();
+
+        $qtWaiting = QueueRequest::where('classroom_id','=',$assistance->classroom_id)
+            ->where('id','<=',$assistance->id)
+            ->whereNull('ended_at')
+            ->count();
+        $position = ["position" => $qtWaiting];
+
+        return response()->json($position, 200);
+    }
+
+    public function updateStatus(Request $request)
+    {
+        $token_id = $request->token_id;
+        if(!Auth::attempt(['token_id' => $token_id, 'password' => $token_id])){
+            return response()->json(['message' => 'user not found'], 401);
+        }
+        $user = Auth::user();
+
+        $presence = StudentsClassroom::where('student_id','=',$user->id)->first();
+
+        $qtWaiting = QueueRequest::where('classroom_id','=',$presence->classroom_id)
+            ->where('id','<=',$presence->student_id)
+            ->whereNull('ended_at')
+            ->count();
+            
+        $position = ["position" => $qtWaiting];
+
+        return response()->json($position, 200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -5,21 +5,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ClassroomController;
 
+Route::middleware('auth:sanctum')->get('/bearer_token_user', function (Request $request) {});
+
+
+
 Route::post('/create-class', [ClassroomController::class, 'createClassroom']);
-// Route::patch('/next', [ApiController::class, 'assistNextStudent']);
-// Route::patch('/stop', [ApiController::class, 'stopAssist']);
+// Route::patch('/next', [ClassroomController::class, 'assistNextStudent']);
+// Route::patch('/stop', [ClassroomController::class, 'stopAssist']);
 
 // Student Routes
 Route::get('/search-class/{id}', [ClassroomController::class, 'searchClassroom']);
 Route::post('/join/{id}', [ClassroomController::class, 'joinClassroom']);
-// Route::patch('/ask', [ApiController::class, 'askAssistance']);
+Route::post('/ask', [ClassroomController::class, 'askAssistance']);
 
 // Professor/Student Route
 Route::get('/users', [UserController::class, 'allUsers']);
 Route::get('/user', [UserController::class, 'user']);
 Route::post('/user', [UserController::class, 'LoginOrRegister']);
-// Route::get('/update', [ApiController::class, 'updateStatus']);
-// Route::get('/exit', [ApiController::class, 'exitClassroom']);
+Route::get('/update', [ClassroomController::class, 'updateStatus']);
+// Route::get('/exit', [ClassroomController::class, 'exitClassroom']);
 
-// Route::get('/reports', [ApiController::class, 'listReports']);
-// Route::get('/report', [ApiController::class, 'getReport']);
+// Route::get('/reports', [ReportController::class, 'listReports']);
+// Route::get('/report', [ReportController::class, 'getReport']);
