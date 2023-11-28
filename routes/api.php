4acde306273e34d040793358c20445aa454192f6
Alexandre Coelho <alexandre.010c@gmail.com>
2023-11-28 15:25:55 -0300

    auth sanctum to CORS policy

--- routes/api.php
+++ routes/api.php
@@ -5,24 +5,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ClassroomController;
 
-Route::middleware('auth:sanctum')->get('/bearer_token_user', function (Request $request) {});
+Route::middleware('auth:sanctum')->get('/bearer_token_user', function (Request $request) {
+    return Auth::user();
+});
 
 
 
-Route::post('/create-class', [ClassroomController::class, 'createClassroom']);
+Route::middleware('auth:sanctum')->post('/create-class', [ClassroomController::class, 'createClassroom']);
 // Route::patch('/next', [ClassroomController::class, 'assistNextStudent']);
 // Route::patch('/stop', [ClassroomController::class, 'stopAssist']);
 
 // Student Routes
-Route::get('/search-class/{id}', [ClassroomController::class, 'searchClassroom']);
-Route::post('/join/{id}', [ClassroomController::class, 'joinClassroom']);
-Route::post('/ask', [ClassroomController::class, 'askAssistance']);
+Route::middleware('auth:sanctum')->get('/search-class/{id}', [ClassroomController::class, 'searchClassroom']);
+Route::middleware('auth:sanctum')->post('/join/{id}', [ClassroomController::class, 'joinClassroom']);
+Route::middleware('auth:sanctum')->post('/ask', [ClassroomController::class, 'askAssistance']);
+Route::middleware('auth:sanctum')->get('/update', [ClassroomController::class, 'updateStatus']);
 
 // Professor/Student Route
 Route::get('/users', [UserController::class, 'allUsers']);
 Route::get('/user', [UserController::class, 'user']);
 Route::post('/user', [UserController::class, 'LoginOrRegister']);
-Route::get('/update', [ClassroomController::class, 'updateStatus']);
 // Route::get('/exit', [ClassroomController::class, 'exitClassroom']);
 
 // Route::get('/reports', [ReportController::class, 'listReports']);
